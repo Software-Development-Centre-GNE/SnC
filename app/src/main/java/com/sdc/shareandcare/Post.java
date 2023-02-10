3ac5238413f4cbe871480f2012094e5383c0fa4e
geekabhinav007 <abhinav26january@gmail.com>
2023-02-11 05:28:11 +0530

    Added 3 Functionality of Post, Web views file

---
+++ app/src/main/java/com/sdc/shareandcare/Post.java
@@ -0,0 +1,58 @@
+package com.sdc.shareandcare;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Post implements Parcelable {
+    private String url, note;
+
+    public Post() {
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    protected Post(Parcel in) {
+        note = in.readString();
+        url = in.readString();
+    }
+
+    public static final Creator<Post> CREATOR = new Creator<Post>() {
+        @Override
+        public Post createFromParcel(Parcel in) {
+            return new Post(in);
+        }
+
+        @Override
+        public Post[] newArray(int size) {
+            return new Post[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(url);
+        dest.writeString(note);
+    }
+}
