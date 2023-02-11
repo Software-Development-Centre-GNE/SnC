de5d7d4df7170807e694c1c63c9b9d4345c574ad
geekabhinav007 <abhinav26january@gmail.com>
2023-02-11 05:53:02 +0530

    Changed files to add multidex enable

--- app/src/main/java/com/sdc/shareandcare/UploadedMediaActivity.java
+++ app/src/main/java/com/sdc/shareandcare/UploadedMediaActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.manandhiman.sdcproject.databinding.ActivityUploadedMediaBinding;
+import com.sdc.shareandcare.databinding.ActivityUploadedMediaBinding;
 
 import java.util.ArrayList;
 import java.util.HashMap;
