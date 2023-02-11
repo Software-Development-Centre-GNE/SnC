768814cf557eca1cada02f8351df72ff101c2a85
geekabhinav007 <abhinav26january@gmail.com>
2023-02-11 05:53:02 +0530

    Changed files to add multidex enable

--- app/src/main/java/com/sdc/shareandcare/MainActivity.java
+++ app/src/main/java/com/sdc/shareandcare/MainActivity.java
@@ -17,7 +17,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.manandhiman.sdcproject.databinding.ActivityMainBinding;
+import com.sdc.shareandcare.databinding.ActivityMainBinding;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
